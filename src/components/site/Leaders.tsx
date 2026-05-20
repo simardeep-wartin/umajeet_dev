06a2339027a88ceca9ff3e1040f36ab31f0008a6
simardeepwartin <simardeep@wartinlabs.com>
2026-05-20 16:43:12 +0530

    Add lift and shadow hover effect to all image cards

--- src/components/site/Leaders.tsx
+++ src/components/site/Leaders.tsx
@@ -93,7 +93,7 @@ export function Leaders() {
               </div>
             </div>
           </div>
-          <div className="border border-[#d9d9d9] rounded-3xl px-10 py-4">
+          <div className="border border-[#d9d9d9] rounded-3xl px-10 py-4 transition-all duration-300 hover:-translate-y-3 hover:shadow-2xl">
             <img
               src={leader.src}
               alt={l.name}
